09272893953f8b4e7b775e7b1e57417641be8836
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-07-21 21:33:34 +0900

    props,prop-types

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,9 +1,36 @@
+import PropTypes from "prop-types";
+
 function App() {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName" },
+  ];
+
   return (
     <div className="App">
-      <h1>Hello,world!</h1>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
   );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      Hi,I am {props.name}, and {props.age} years old!
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 0,
+};
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
+};
 
 export default App;
